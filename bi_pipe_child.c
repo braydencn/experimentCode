0cdca8d4f5a6fc5e18f7408a9bf2ddd8dea90837
braydenCN <pczhang2010@gmail.com>
2013-12-11 07:42:46 -0500

    first commit

---
+++ bi_pipe_child.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main()
+{
+	int input;
+	scanf("%d", &input);
+	printf("input number is %d\n", input);
+	return 0;
+}
---
+++ bi_pipe_parent.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void err_sys(char* msg)
+{
+	perror(msg);
+	exit(-1);
+}	
+
+int
+main()
+{
+	int fd[2]; //write by parent, read by child
+	int fd1[2]; //write by child, read by parent 
+	int pid;
+	if(pipe(fd) < 0)
+		err_sys("pipe error");
+	if(pipe(fd1) < 0)
+		err_sys("pipe1 error");
+	if((pid = fork()) < 0)
+		err_sys("fork error");
+	else if(pid > 0){//parent
+		close(fd[0]); //not read fd
+		close(fd1[1]); //not write fd1
+		if(write(fd[1], "2\n", 2) < 0)
+			err_sys("write error");
+		char tmp[255];
+		int n;
+		if((n = read(fd1[0], tmp, 255)) < 0)
+			err_sys("read error");
+		if(write(1, tmp, n) < 0)
+			err_sys("write error");
+	}else{ //child
+		close(0);
+		close(1);
+		close(fd[1]); //not write fd
+		close(fd1[0]); //not read fd1
+		dup2(fd[0], 0);
+		dup2(fd1[1], 1);
+		if(execl("./child", NULL) < 0)
+			err_sys("exec error");
+	}
+	if((waitpid(pid, NULL, 0) < 0))
+		err_sys("wait_pid error");
+	return 0;
+}
